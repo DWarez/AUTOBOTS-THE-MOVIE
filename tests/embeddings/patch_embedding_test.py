5e966115d9335cbaa2ef0cc3ff3d5088219e2912
DWarez <ldwarezl@gmail.com>
2022-06-17 18:57:52 +0200

    better test unit

--- tests/embeddings/patch_embedding_test.py
+++ tests/embeddings/patch_embedding_test.py
@@ -7,4 +7,6 @@ from patch_embedding import PatchEmbedding
 pe = PatchEmbedding(320)
 _tensor = torch.randn(10, 3, 320, 320)
 
-print(pe(_tensor).shape)
+out_shape = pe(_tensor).shape
+
+assert out_shape == (10, 401, 768), f"Incorrect embedding shape, expected (10, 401, 768), got {out_shape}"
