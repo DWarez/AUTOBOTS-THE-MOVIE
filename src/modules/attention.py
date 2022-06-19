95e04fc042444f32e9f81ee1c5d5c9f425d9ac39
DWarez <ldwarezl@gmail.com>
2022-06-19 12:02:51 +0200

    Introduced hidden dimension (different from embedd
    ing size) in attention mechanism. Also changed scaling.

--- src/modules/attention.py
+++ src/modules/attention.py
@@ -1,5 +1,3 @@
-from turtle import forward
-from einops import rearrange
 import torch
 import torch.nn as nn
 
@@ -7,25 +5,39 @@ from einops import rearrange
 
 
 class Attention(nn.Module):
-    def __init__(self, n_heads: int = 8, embedding_size: int = 512,
-                                            dropout_prob: float = 0.1) -> None:
+    def __init__(self, 
+                    n_heads: int = 8, 
+                    d_head: int = 64,
+                    embedding_size: int = 512,
+                    dropout_prob: float = 0.1) -> None:
         """Multi Head Attention module
 
         Args:
             n_heads (int, optional): Number of heads. Defaults to 8.
+            d_head (int,  optional): Size of the heads. Defaults to 64.
             embedding_size (int, optional): Embedding size. Defaults to 512.
             dropout_prob (float, optional): Dropout probability. 
                 Defaults to 0.1.
         """
         super(Attention, self).__init__()
         self.n_heads = n_heads
+        self.d_head = d_head
         self.embedding_size = embedding_size
         self.dropout_prob = dropout_prob
 
-        self.projection = nn.Linear(self.embedding_size, self.embedding_size)
-        self.linear_q = nn.Linear(self.embedding_size, self.embedding_size)
-        self.linear_k = nn.Linear(self.embedding_size, self.embedding_size)
-        self.linear_v = nn.Linear(self.embedding_size, self.embedding_size)
+        self.d_hidden = self.n_heads * self.d_head
+
+        self.projection_condition = not (self.n_heads == 1 and 
+                                            self.d_head == self.embedding_size)
+        self.projection =\
+             nn.Sequential(
+                nn.Linear(self.d_hidden, self.embedding_size),
+                nn.Dropout(dropout_prob)
+            ) if self.projection_condition else nn.Identity()
+
+        self.linear_q = nn.Linear(self.embedding_size, self.d_hidden)
+        self.linear_k = nn.Linear(self.embedding_size, self.d_hidden)
+        self.linear_v = nn.Linear(self.embedding_size, self.d_hidden)
         self.dropout = nn.Dropout(self.dropout_prob)
         self.softmax = nn.Softmax(-1)
         
@@ -55,7 +67,7 @@ class Attention(nn.Module):
         if mask is not None:
             scores.masked_fill_(mask==0, -1e-12)
 
-        A = self.softmax(scores)/(self.embedding_size ** 1/2)
+        A = self.softmax(scores * self.d_head ** -0.5)
         A = self.dropout(A)
 
         # [batch_size, n_heads, l_values, embedding_size]
--- tests/modules/attention_test.py
+++ tests/modules/attention_test.py
@@ -12,6 +12,7 @@ pe = PatchEmbedding(224)
 _tensor = torch.randn(10, 3, 224, 224)
 
 embedding = pe(_tensor)
+
 out_shape = mha(embedding, None).shape
 
 assert out_shape == (10, 197, 768), f"Incorrect embedding shape, expected (10, 197, 768), got {out_shape}"
